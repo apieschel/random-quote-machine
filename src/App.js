c982cbb4b70a5d82756785814c3229a59eab4264
apieschel <apieschel@gmail.com>
2018-11-13 22:29:41 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -9,16 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            &ldquo;After some further crooked questions from these bourgeois, the secret of their 'sympathy' for widows, poor families and so on emerges into the daylight.&rdquo;
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
